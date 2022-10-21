272157598ba50de74a1f5688f7a684f92faf1b61
marcelo gomes <msilvagomes2009@gmail.com>
2022-10-21 10:53:20 -0300

    modifquei de novo

--- teste c.cpp
+++ teste c.cpp
@@ -5,7 +5,15 @@
 
 int main (){
 	
-	printf("quase dormindo");
+	int num, num2, soma;
+	
+	printf("digite primeiro valor para soma: \n");
+	scanf("%i", num);
+	printf("digite segundo valor para soma: \n");
+	scanf("%i", num2);
+	
+	soma=num+num2;
+	printf("\nsoma =: \n",soma);
 	
 	
 	return 0;
